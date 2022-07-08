8d4712c835465827cfac947d4701374c5a606714
GabrielDragone <gabriel.dragone@live.com>
2022-07-08 16:16:20 -0300

    Adicionados exemplos de como usar Selector, que é um filtro para que o Consumidor veja se vai ou nao consumir mensagem enviada pelo Produtor onde tem as configurações de headers

---
+++ src/main/java/topic/TesteConsumidorEstoqueTopicoSelector.java
@@ -0,0 +1,48 @@
+package topic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import java.util.Scanner;
+
+public class TesteConsumidorEstoqueTopicoSelector {
+
+    // Vai receber a mensagem mesmo se o Consumidor estiver offline, pois foi feito a configuração seClienteID e assinatura.
+    // Selector: Quando o sistema não está interessado em todas as mensagens.
+    public static void main(String[] args) throws Exception {
+        InitialContext initialContext = new InitialContext();
+        ConnectionFactory connectionFactory = (ConnectionFactory)initialContext.lookup("ConnectionFactory");
+        Connection connection = connectionFactory.createConnection();
+        connection.setClientID("estoque");
+
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Topic topico = (Topic) initialContext.lookup("loja"); // O topico de dentro do jndi.
+        MessageConsumer messageConsumer = session.createDurableSubscriber(topico,
+                "assinatura",
+                "ebook=false OR ebook is null", // MessageSelector: Só irá receber mensagens produzidas pelo Produtor com header informando o ebook=false.
+                false);
+
+        messageConsumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage)message;
+                try {
+                    System.out.println("TesteConsumidorEstoqueTopicoSelector: " + textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        System.out.println("[TesteConsumidorEstoqueTopicoSelector] Teste Antes do nextLine()");
+        new Scanner(System.in).nextLine();
+        System.out.println("[TesteConsumidorEstoqueTopicoSelector] Teste Depois do nextLine()");
+
+        session.close();
+        connection.close();
+        initialContext.close();
+    }
+}
---
+++ src/main/java/topic/TesteProdutorTopicoSelector.java
@@ -0,0 +1,45 @@
+package topic;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import java.util.Scanner;
+
+public class TesteProdutorTopicoSelector {
+
+    // Exemplo de ENVIO de mensagens com SELECTOR. Posteriormente usar a classe TesteConsumidorComMessageListenerTopico para testar o recebimento:
+    public static void main(String[] args) throws Exception {
+        InitialContext initialContext = new InitialContext();
+        ConnectionFactory connectionFactory = (ConnectionFactory)initialContext.lookup("ConnectionFactory");
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = (Destination) initialContext.lookup("loja");
+        MessageProducer messageProducer = session.createProducer(destination);
+
+        for (int i = 0; i < 100 ; i++) {
+            Message message;
+            if (i % 2 == 0) {
+                message = session.createTextMessage("<pedido><id>" + i + "</id><ebook>true</ebook></pedido>");
+                message.setBooleanProperty("ebook", true); // header, esse não deve ser entregue
+            } else {
+                message = session.createTextMessage("<pedido><id>" + i + "</id><ebook>false</ebook></pedido>");
+                //message.setBooleanProperty("ebook", false); // header, esse deve ser entregue
+            }
+            messageProducer.send(message); // Esse cara faz o envio da mensagem para o topico.
+        }
+
+        System.out.println("[TesteProdutorTopicoSelector] Teste Antes do nextLine()");
+
+        // Deixa a aplicação "pausada" enquanto algo não é digitado no teclado:
+        new Scanner(System.in).nextLine();
+
+        System.out.println("[TesteProdutorTopicoSelector] Teste Depois do nextLine()");
+
+        session.close();
+        connection.close();
+        initialContext.close();
+
+    }
+
+}
